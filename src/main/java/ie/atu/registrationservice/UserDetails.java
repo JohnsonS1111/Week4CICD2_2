6f15d9397d2f89b318155a76cce077d951acef92
Johnson <g00389668@ATU.IE>
2023-10-17 00:44:09 +0100

    Created Registration Service Application openfeign connection

---
+++ src/main/java/ie/atu/registrationservice/UserDetails.java
@@ -0,0 +1,13 @@
+package ie.atu.registrationservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDetails {
+    private String email;
+    private String name;
+}
